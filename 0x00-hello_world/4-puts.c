fdad42cb92c85f214d7bc16d157ba221ac7145d7
KarishB14 <kbudhu14@gmail.com>
2022-06-02 13:29:53 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,29 +1,25 @@
-#include<stdio.h>
-
-
+#include <stdio.h>
 
 /**
  *
- *  * main - Entry point
+ *    * main - print to screen
  *
- *   *
+ *       *
  *
- *    * Description: A C program that prints with printf function.
+ *          * Return: zero
  *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *      */
-
-
+ *             */
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
-			return (0);
+
+
+			puts(Mystr);
+
+				return (0);
 
 }
