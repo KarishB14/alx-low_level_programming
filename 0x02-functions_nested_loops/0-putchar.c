24f4a7ed6807981a720a20f1dc2dc9d0b6b0bf70
KarishB14 <kbudhu14@gmail.com>
2022-06-07 14:27:38 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,12 @@
-#include "main.h"
-
 /**
- * main - prints _putchar
- *
- * Return: 0 (Success)
+ * main - prints to string
+ * Description: Prints positive, negative or zero
+ * Return: 0
  */
-
+#include <stdio.h>
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+printf("_putchar");
+putchar ('\n');
+return (0);
 }
