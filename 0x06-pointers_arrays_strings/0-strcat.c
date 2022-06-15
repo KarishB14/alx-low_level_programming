87ba8d8b02dbca5fefc3dbc52aa5e50dbeb24073
KarishB14 <kbudhu14@gmail.com>
2022-06-15 13:05:39 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,16 @@
 
 char *_strcat(char *dest, char *src)
 {
+int len = strlen(dest), i, n = strlen(src);
+
+for (i = 0 ; i <= n /*&& *src != '\0' */; i++)
+{
  /* printf("%d\n", i); */
 dest[len + i] = src[i];
 /* src++; */
 /* dest++; */
 }
 /* printf("i = %d", i); */
+/* dest[len + i] = '\0'; */
 return (dest);
 }
