10d76c86042ba0ccd01373bd4bc9a740d619634c
KarishB14 <kbudhu14@gmail.com>
2022-06-26 12:01:49 -0700

    task 0

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,24 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/* #include <string.h> */
+#include <stdio.h>
+/* #include <stddef.h> */
+
 /**
- * _atoi - converts a string to an integer
- *
- * @s: string input parameter
- *
- * Return: converted integer from string
+ * _atoi - function that convert a string to an integer.
+ * @s:pointer of caracter
+ * Return: integer
  */
 
 int _atoi(char *s)
 {
-	unsigned int num = 0;
-	int sign = 1;
+	int sum = 0;
+	/**
+	 * On somme toutes les valeurs passées en arguments du programme.
+	 * Pour ce faire on déplace le pointeur argv pour scanner tous
+	 * les arguments
+	 */
+	while (*s != 0)
+	{
+		/* printf ("s = %d\n", *s); */
+		sum = atoi(s + '0');
+		s++;
+		printf ("sum = %d\n", sum);
+	}
+	printf ("sum = %d\n", sum);
+		 /* int a = atoi( *sum);*/
 
-	do {
-		if (*s == '-')
-			sign *= -1;
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-		else if (num > 0)
-			break;
-	} while (*s++);
-
-	return (num * sign);
+	return (0);
 }
