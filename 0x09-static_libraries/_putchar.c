ea9cc7607ee9efeb12b86e91da76898ea6970bb4
KarishB14 <kbudhu14@gmail.com>
2022-06-28 13:10:22 -0700

    task

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,12 +1,13 @@
+#include <unistd.h>
+
 /**
- * main - prints to string
- * Description: Prints positive, negative or zero
- * Return: 0
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-#include <stdio.h>
-int main(void)
+int _putchar(char c)
 {
-printf("_putchar");
-putchar ('\n');
-return (0);
+	return (write(1, &c, 1));
 }
